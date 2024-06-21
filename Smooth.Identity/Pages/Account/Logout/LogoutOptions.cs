fcb1506e34ca31c60b7fe1a7f1d4aec8d271c336
EricJansen <EricJansen@LAPTOP-EJ>
2024-06-21 15:05:50 -0400

    Add project files.

---
+++ Smooth.Identity/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+namespace Smooth.Identity.Pages.Logout
+{
+    public static class LogoutOptions
+    {
+        public static readonly bool ShowLogoutPrompt = true;
+        public static readonly bool AutomaticRedirectAfterSignOut = false;
+    }
+}
